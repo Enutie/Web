408c709f073b876196dff64278ef3659928fd5e0
Morten <enutie@gmail.com>
2025-08-06 22:23:45 +0200

    added very hansome work in progress page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>Welcome.</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Work in progress - When I know what to build, so no eta. 
   </p>
+  <p>
+    In the meantime, you can enjoy some of my other sites:
+  </p>
+  <ul>
+    <li><a href="https://blog.enutie.com">My blog</a></li>
+    <li><a href="https://Sketchedular.enutie.com">My warm-up generator for drawabox</a></li>
+    <li><a href="https://pong.enutie.com">My take on the classic - Pong</a></li>
+    <li><a href="https://Enutie.github.io">My Bachelor Project from my time at the IT University of Copenhagen</a></li>
+  </ul>
 </template>
 
 <style scoped></style>
